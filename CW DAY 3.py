f68f5afb7f0b865dcf3b9c93bdbe2fddd3ccfe91
Ardrams2244 <ardrams2710@gmail.com>
2026-01-02 20:50:09 +0530

    day 3

---
+++ CW DAY 3.py
@@ -0,0 +1,14 @@
+has_account=True
+email_verified=False
+can_login=has_account and email_verified
+email= "g@example.com"
+is_email_valid="@" in email
+user_age=17
+is_age_valid=user_age>=18
+print(is_age_valid)
+can_login_final=(has_account and email_verified and is_email_valid and is_age_valid)
+print(has_account)
+print(email_verified)
+print(is_email_valid)
+print(is_age_valid)
+print("has_account is true:", has_account is True)
