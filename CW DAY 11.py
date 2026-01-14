e9e61ea7d7d159d2db2dd85e23006519f9004245
Ardrams2244 <ardrams2710@gmail.com>
2026-01-14 15:11:22 +0530

    dayy 11

--- CW DAY 11.py
+++ CW DAY 11.py
@@ -5,7 +5,7 @@ trips=[create_trip("Mumbai","16-08-2025","City of dreams"),
       create_trip("Jaipur","18-05-2025","Pink city"),
       create_trip("Hyderabad","20-12-2025","City of pearls")]
 for trip in trips:
-    date_obj=datetime.strptime(trip["date_visited"],"%d-%m-%y")
+    date_obj=datetime.strptime(trip["date_visited"],"%d-%m-%y").date()
     trip["date_visited"]=date_obj.strftime("%B %d,%y")
 json_data=json.dumps(trips,indent=4)
 print(json_data)
